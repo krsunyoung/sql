a68540d48c5391451c08b1cb0efeb1ed4fd0c1b9
sunyoung <bit-user1@bit-user>
2016-10-06 17:26:44 +0900

    1006

--- practice1/practice04.sql
+++ practice1/practice04.sql
@@ -23,4 +23,13 @@ where e.DEPARTMENT_ID = b.department_id
 	and e.SALARY>b."평균"
 order by b.department_id;
 
+--4-1번 자신의 부서의 평균 연봉보다 크거나 같은 직원들의 사번, 부서넘버, 이름, 연봉 출력
+select e.department_id, e.employee_id, e.last_name, e.salary
+from EMPLOYEES e ,
+	(select department_id,0 , avg(salary) as "평균"  from employees group by department_id) b
+where nvl(e.DEPARTMENT_ID,0)  = nvl(b.department_id,0)    
+	and e.SALARY>=b."평균"
+	order by e.department_id;
+--null값을 나오게 하기 위해서는 어떻게 해야하는가 ?
+--where 절에 nvl 을 넣어줌으로써 null값이 나옴. 만약 nvl을 써주지 않으면 null값은 비교를 할수 없어서 값에 출력 되지 않는다.
 
